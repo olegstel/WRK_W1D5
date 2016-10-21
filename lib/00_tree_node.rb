377fc4e3c9c2a4fc317628303e6ba6547c14a64a
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2016-10-21 13:34:33 -0400

    Initialize nodes

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,23 @@
 class PolyTreeNode
+  attr_reader :parent, :value, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+
+    #
+    @parent = node
+    node.children << self   unless node.nil? ||  node.children.include?(self)
+  end
+
+
+
+
+
+
 
 end
