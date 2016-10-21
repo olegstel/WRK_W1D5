3a4ecfeb004ab144f0afee98c58b012decf9b8dd
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2016-10-21 15:49:33 -0400

    Add dfs and bfs methods

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -15,11 +15,48 @@ class PolyTreeNode
 
     unless old_parent == node
       @parent = node
+
       node.children << self   unless node.nil? ||  node.children.include?(self)
-      # debugger unless old_parent.nil?
       unless old_parent.nil?
         old_parent.children.delete(self)
       end
+    end
+
+    def add_child(node)
+      node.parent = self
+
+      unless self.children.include?(node)
+        self.children << node
+      end
+    end
+
+    def remove_child(node)
+      raise "Not a child " unless self.children.include?(node)
+      self.children.delete(node)
+      node.parent = nil
+    end
+
+    def dfs(target_value)
+      return self if self.value == target_value
+
+      self.children.each do |child|
+        search_result = child.dfs(target_value)
+        return search_result unless search_result.nil?
+      end
+
+      nil
+    end
+
+    def bfs(target_value)
+      nodes =[self]
+
+      until nodes.empty?
+        last_node = nodes.pop
+        return last_node if last_node.value == target_value
+        last_node.children.each { |ch| nodes.unshift(ch) } unless last_node.children.nil?
+      end
+
+      nil
     end
 
   end
